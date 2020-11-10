54bb9536416b38ecb990b50b2e7036905ffd4085
pepperoni92 <jlapegna@gmail.com>
2020-11-09 16:40:22 -0800

    Added basic physics to move the kart

---
+++ Source/KrazyKarts/GoKart.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoKart.h"
+
+// Sets default values
+AGoKart::AGoKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+}
+
+// Called when the game starts or when spawned
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
+	FVector Translation = Velocity * DeltaTime * 100.f; // x100 because we need to go from cm to m
+
+	FHitResult HitResult;
+	AddActorWorldOffset(Translation, true, &HitResult);
+
+	if (HitResult.IsValidBlockingHit())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HIT SOMETHING"));
+		Velocity = FVector::ZeroVector;
+	}
+}
+
+void AGoKart::ApplyRotation(float DeltaTime)
+{
+	// Get rotation
+	float RotationAngle = MaxDegreesPerSecond * DeltaTime * SteeringThrow;
+	FQuat RotationDelta(GetActorUpVector(), FMath::DegreesToRadians(RotationAngle));
+
+	Velocity = RotationDelta.RotateVector(Velocity);
+
+	AddActorWorldRotation(RotationDelta, true);
+}
+
+FVector AGoKart::GetResistance()
+{
+	return -Velocity.GetSafeNormal() * Velocity.SizeSquared() * DragCoefficient;
+}
+
+// Called every frame
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Force based on input from player
+	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+	// Calculate and add the air resistance to the force
+	Force += GetResistance();
+	
+	FVector Acceleration = Force / Mass;
+	Velocity = Velocity + Acceleration * DeltaTime;
+	
+	ApplyRotation(DeltaTime);
+	UpdateLocationFromVelocity(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
+}
+
+void AGoKart::MoveForward(float Value)
+{
+	Throttle = Value;
+}
+
+void AGoKart::MoveRight(float Value)
+{
+	SteeringThrow = Value;
+}
+
---
+++ Source/KrazyKarts/GoKart.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GoKart.generated.h"
+
+UCLASS()
+class KRAZYKARTS_API AGoKart : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AGoKart();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	void UpdateLocationFromVelocity(float DeltaTime);
+	void ApplyRotation(float DeltaTime);
+	FVector GetResistance();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	// Mass of the car in kilograms (kg)
+	UPROPERTY(EditAnywhere)
+	float Mass = 1000;
+	
+	UPROPERTY(EditAnywhere)
+	float MaxDrivingForce = 10000;
+
+	// Number of degrees rotated per second at full SteeringThrow
+	UPROPERTY(EditAnywhere)
+	float MaxDegreesPerSecond = 90;
+
+	// amount of resistance to apply, kg/m
+	UPROPERTY(EditAnywhere)
+	float DragCoefficient = 16; // 10,000 / 25*25
+
+	float Throttle;
+	float SteeringThrow;
+	
+	FVector Velocity;
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	
+};
